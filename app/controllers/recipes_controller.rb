ff8923d7d921846436670596a31ff864b9172ac8
Louis Brutton <louisbrutton@gmail.com>
2016-09-04 20:02:33 +0200

    finish the assignement

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || "chocolate"
-  	@recipes = Recipe.for(@search_term)
+  	@search = params[:search] || "chocolate"
+  	@recipes = Recipe.for(@search)
+
   end
 end
